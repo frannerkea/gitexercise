5dee85f58386dc784d76c3241b261ab8989a9049
frannerkea <chri268u@stud.kea.dk>
2022-09-09 13:45:13 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
 
     public void start() {
         System.out.println("det virker!");
+        udskrivNavn();
     }
+
+    public void udskrivNavn() {
+        System.out.println("Hvad er dit navn?");
+        System.out.println("Skriv did navn: ");
+        Scanner brugerInput = new Scanner(System.in);
+        String ditNavn = brugerInput.nextLine();
+        System.out.print("Hej" + ditNavn);
+    }
+
+
+    //          Scanner brugerInput = new Scanner(System.in);
+    //        int antalPersoner = brugerInput.nextInt();
+    //        opskrift.setAntalPersoner(antalPersoner);
+
 
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+
     }
+
 
 }

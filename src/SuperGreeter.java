4dcf397378919410e92720b914638a3e5abad0a2
frannerkea <chri268u@stud.kea.dk>
2022-09-09 14:17:09 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -4,15 +4,25 @@ public class SuperGreeter {
 
     public void start() {
         System.out.println("det virker!");
-        udskrivNavn();
+        printName();
+        askAboutAge();
     }
 
-    public void udskrivNavn() {
-        System.out.println("Hvad er dit navn?");
-        System.out.println("Skriv did navn: ");
-        Scanner brugerInput = new Scanner(System.in);
-        String ditNavn = brugerInput.nextLine();
-        System.out.print("Hej med dig! " + ditNavn);
+    public void printName() {
+        System.out.println("What is your name?");
+        System.out.println("Write your name: ");
+        Scanner userName = new Scanner(System.in);
+        String yourName = userName.nextLine();
+        System.out.print("Hej med dig! " + yourName);
+        System.out.println(" ");
+    }
+
+    public void askAboutAge() {
+        System.out.println("How old is u? ");
+        Scanner userAge = new Scanner(System.in);
+        int age = userAge.nextInt();
+        System.out.println("okay you are " + age + " old");
+
     }
 
 
@@ -24,6 +34,7 @@ public class SuperGreeter {
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+
 
     }
 

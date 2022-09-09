a0888d01bb21086b4e432b3cffb78b9e93932998
frannerkea <chri268u@stud.kea.dk>
2022-09-09 13:35:19 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,12 @@
+public class SuperGreeter {
+
+    public void start() {
+        System.out.println("det virker!");
+    }
+
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+    }
+
+}
